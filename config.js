07a68483100d52ac8501ac09674dd6747c01d111
Bismarck Villca Soliz <bismarck.villca@gmail.com>
2022-07-11 02:46:57 -0400

    dockerize node backend

--- config.js
+++ config.js
@@ -1,25 +1,24 @@
 const API_VERSION = "v1";
 const IP_SERVER_API = "localhost";
-const IP_SERVER_DB = "ec2-54-157-16-196.compute-1.amazonaws.com"; //localhost
-// const IP_SERVER_DB = "localhost"; //localhost
-const PORT_DB = 5432;
-const PORT_SERVER = process.env.PORT || 3977; // 3977 || 5290
-const PARAMS_CONNECTION = {
-  host: IP_SERVER_DB,
-  user: "svtwlzpatpgvdg",
-  password: "71b620b8896975475949c07a703f156fe6cc96a6b2b2d1042319fbd9c410ae1c", //apsadmin2022
-  database: "dfpc86sb3fh4vd",
-  port: PORT_DB,
-  ssl: { rejectUnauthorized: false },
-};
-
+const IP_SERVER_DB = process.env.DATABASE_HOST || "localhost";
+const PORT_DB = process.env.DATABASE_PORT || 5432;
+const PORT_SERVER = 3977; // 3977 || 5290
 // const PARAMS_CONNECTION = {
 //   host: IP_SERVER_DB,
-//   user: "postgres",
-//   password: "navau", //apsadmin2022
-//   database: "APS",
+//   user: process.env.DATABASE_USER || "svtwlzpatpgvdg",
+//   password: process.env.DATABASE_PASS || "71b620b8896975475949c07a703f156fe6cc96a6b2b2d1042319fbd9c410ae1c", //apsadmin2022
+//   database: process.env.DATABASE_NAME || "dfpc86sb3fh4vd",
 //   port: PORT_DB,
+//   ssl: { rejectUnauthorized: false },
 // };
+
+const PARAMS_CONNECTION = {
+  host: IP_SERVER_DB,
+  user: process.env.DATABASE_USER || "postgres",
+  password: process.env.DATABASE_PASS || "navau", //apsadmin2022
+  database: process.env.DATABASE_NAME || "APS",
+  port: PORT_DB,
+};
 
 module.exports = {
   API_VERSION,
