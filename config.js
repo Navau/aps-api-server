f9d4daed6c23dd9b2311b8eb3cedc15a5b5348af
Navau <milibrolunadepluton344@gmail.com>
2022-07-11 12:37:27 -0400

    Avances 11/7/22 de nuevo X3

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const API_VERSION = "v1";
 const IP_SERVER_API = "localhost";
 const IP_SERVER_DB = "ec2-54-157-16-196.compute-1.amazonaws.com";
 const PORT_DB = 5432;
-const PORT_SERVER = 3977; // 3977 || 5290
+const PORT_SERVER = process.env.PORT || 3977; // 3977 || 5290
 const PARAMS_CONNECTION = {
   host: IP_SERVER_DB,
   user: "svtwlzpatpgvdg",
